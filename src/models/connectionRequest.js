1ff7a6453b9808da5d21ba46dea4a60458cf94e6
agamsidana <sidanaagam@gmail.com>
2026-01-06 08:42:16 +0530

    add schema indexing

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -21,6 +21,8 @@ const connectionRequestSchema=new mongoose.Schema({
     }
 },{timestamps:true});
 
+connectionRequestSchema.index({fromUserId:1,toUserId:1});
+
 connectionRequestSchema.pre('save',function(){
     const connectionRequest=this;
     // check if toUserId is equalto fromUserId
